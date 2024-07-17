97f378b6f4fc954a5b00d39852cc47da1a232fef
ywitha-597 <142316421+ywitha-597@users.noreply.github.com>
2024-07-17 20:00:47 +0530

    Update and rename JumpGame to JumpGame.cpp

--- Medium/JumpGame.cpp
+++ Medium/JumpGame.cpp
@@ -1,5 +1,7 @@
 // Jump Game //
-bool canJump(vector<int>& nums) {
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
        int reach = 0;
         for(int i = 0; i < nums.size();i++){
             if(i <= reach){
@@ -10,3 +12,4 @@ bool canJump(vector<int>& nums) {
         }
         return true;
     }
+};
