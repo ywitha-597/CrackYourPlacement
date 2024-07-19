a95c90419cb6ee0c665c9107068883dc1785d19e
ywitha-597 <142316421+ywitha-597@users.noreply.github.com>
2024-07-19 18:33:13 +0530

    Create DiameterOfBT.cpp

---
+++ Easy/DiameterOfBT.cpp
@@ -0,0 +1,17 @@
+// Diameter of a binary Tree //
+class Solution {
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        int maxi = 0;
+        height(root, maxi);
+        return maxi;
+    }
+private:
+    int height(TreeNode* node, int& maxi){
+        if (node == NULL) return 0;
+        int lh = height(node->left, maxi);
+        int rh = height(node->right, maxi);
+        maxi = max(maxi, lh + rh);
+        return 1 + max(lh, rh);
+    }
+};
